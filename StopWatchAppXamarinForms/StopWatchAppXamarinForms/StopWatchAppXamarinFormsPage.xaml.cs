3350e26b527b9cff52cd8d412094f6e3ca734538
amay077 <okuokuoku@gmail.com>
2017-02-23 23:07:52 +0900

    Xamarin.Formsソリューションを追加

---
+++ StopWatchAppXamarinForms/StopWatchAppXamarinForms/StopWatchAppXamarinFormsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace StopWatchAppXamarinForms
+{
+    public partial class StopWatchAppXamarinFormsPage : ContentPage
+    {
+        public StopWatchAppXamarinFormsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
